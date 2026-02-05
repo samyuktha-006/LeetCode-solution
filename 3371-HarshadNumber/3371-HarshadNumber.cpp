c18018257328d91cfb2a3c691266b592f29f4876
samyuktha-006 <24149048@drngpit.ac.in>
2026-02-05 10:17:50 +0530

    Create 3371-HarshadNumber

---
+++ 3371-HarshadNumber/3371-HarshadNumber.cpp
@@ -0,0 +1,23 @@
+// Last updated: 2/5/2026, 10:17:51 AM
+class Solution {
+public:
+    int sumOfTheDigitsOfHarshadNumber(int x) {
+         int sum = 0;
+        int temp = x;
+        
+       
+        while (temp > 0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+        
+        
+        if (x % sum == 0) {
+            return sum;
+        } else {
+            return -1;
+        }
+
+
+    }
+};
